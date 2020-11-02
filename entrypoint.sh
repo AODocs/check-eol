4ad0f7df671c6c877f09a9e396d39b1d9f0a6351
Laurent RICHARD <easylo@gmail.com>
2020-11-02 13:36:11 +0100

    refacto output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ BOLD_YELLOW='\033[1;33m'
 BOLD_CYAN='\033[1;36m'
 NC='\033[0m'
 
-RESULT=0
+rErrors=()
+rInfos=()
 
-
-files=$(git ls-files --eol)
+files=$(git ls-files --eol $1)
 
 
 # echo "$files"
@@ -19,17 +19,23 @@ files=$(git ls-files --eol)
 while IFS= read -r line; do
 
     file_name=$(echo $line |awk '{ print $NF }')
+    attribute=$(echo $line |awk 'match($0, / attr\/(text|-text) /) { print substr( $0, RSTART+6, RLENGTH-7 )}')
     working_tree=$(echo $line |awk 'match($0, / w\/(lf|crlf|none) /) { print substr( $0, RSTART+3, RLENGTH-4 )}')
     expected=$(echo $line |awk 'match($0, / eol=(lf|crlf|none) /) { print substr( $0, RSTART+5, RLENGTH-6 )}')
 
-    if [[ -z "$working_tree" &&  -z "$expected" ]]
+    # echo "$file_name -- ##$attribute##"
+    if [[ "$attribute" == "text" ]]
     then
-        printf "${BOLD_YELLOW}No EOL rule defined for %s${NC}\n" $file_name
-    else
-        if [ "$working_tree" != "$expected"  ]
-        then 
-            ((RESULT++))
-            printf "Found file ${BOLD_RED}%s${NC} with ${BOLD_CYAN}%s${NC} endings but expected ${BOLD_YELLOW}%s.${NC}\n" $file_name $working_tree $expected            
+        if [[ -z "$working_tree" &&  -z "$expected" ]]
+        then        
+            msg=$( printf "${BOLD_YELLOW}No EOL rule defined for %s${NC}\n" $file_name)
+            rInfos+=("$msg")
+        else
+            if [ "$working_tree" != "$expected"  ]
+            then             
+                msg=$( printf "Found file ${BOLD_RED}%s${NC} with ${BOLD_CYAN}%s${NC} endings but expected ${BOLD_YELLOW}%s.${NC}\n" $file_name $working_tree $expected)
+                rErrors+=("$msg")
+            fi
         fi
     fi
     
@@ -37,10 +43,21 @@ while IFS= read -r line; do
 done <<< "$files"
 
 
-if [ "$RESULT" -eq 0 ]
+for value in "${rInfos[@]}"
+do
+    echo $value
+done
+
+if [ "${#rErrors[@]}" -eq 0 ]
 then
     printf "${BOLD_GREEN}No files with EOL errors found.${NC}\n"
     exit 0
 else
-    exit $RESULT
+    for value in "${rErrors[@]}"
+    do
+        echo $value
+    done
+    printf "${BOLD_YELLOW} %s files with EOL warnings found.${NC}\n" ${#rInfos[@]}
+    printf "${BOLD_RED} %s files with EOL errors found.${NC}\n" ${#rErrors[@]}
+    exit ${#rErrors[@]}
 fi
